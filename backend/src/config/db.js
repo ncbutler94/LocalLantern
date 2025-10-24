cb42f6b00e114d7766a372c22a2e534f00e66adc
Nathan Butler <ncbutler94@gmail.com>
2025-10-24 15:19:07 -0500

    Added gitignore file

--- backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-// backend/src/db.js
-import knexConstructor from 'knex';
-import config from '../../knexfile.cjs';
-
-const env = process.env.NODE_ENV || 'development';
-const knex = knexConstructor(config[env] || config);
-
-export default knex;
--- frontend/src/api/community/generalDiscussion.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/api/community/generalDiscussion.js
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-
-export async function createGeneralDiscussion(formData) {
-    const { data } = await axios.post('/api/general-discussion', formData);
-    return data;             // { id, photos }
-}
